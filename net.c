cf832a13d47ba9176296956a5906ed459a0c67d9
Yuu Yamanoi <yuu.yamanoi1222@gmail.com>
2023-08-21 00:11:09 +0900

    impl step3

--- net.c
+++ net.c
@@ -112,6 +112,12 @@ int
 net_run(void)
 {
   struct net_device *dev;
+
+  if (intr_run() == -1) {
+    errorf("intr_run() failure");
+    return -1;
+  }
+
   debugf("open all devices...");
 
   for (dev = devices; dev; dev = dev->next) {
@@ -130,12 +136,17 @@ net_shutdown(void)
   for (dev = devices; dev; dev = dev->next) {
     net_device_close(dev);
   }
+  intr_shutdown();
   debugf("shutting down");
 }
 
 int
 net_init(void)
 {
+  if (intr_init() == -1) {
+    errorf("intr_init() failure");
+    return -1;
+  }
   infof("initialized");
   return 0;
 }
