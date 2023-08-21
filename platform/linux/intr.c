9f7e8d3db353faabe824ae123129499481c2952c
Yuu Yamanoi <yuu.yamanoi1222@gmail.com>
2023-08-21 23:32:22 +0900

    impl step5

--- platform/linux/intr.c
+++ platform/linux/intr.c
@@ -86,6 +86,9 @@ intr_thread(void *arg)
       case SIGHUP:
         terminate = 1;
         break;
+      case SIGUSR1:
+        net_softirq_handler();
+        break;
       default:
         for (entry = irqs; entry; entry = entry->next) {
           if (entry->irq == (unsigned int)sig) {
@@ -137,5 +140,6 @@ intr_init(void)
   pthread_barrier_init(&barrier, NULL, 2);
   sigemptyset(&sigmask);
   sigaddset(&sigmask, SIGHUP);
+  sigaddset(&sigmask, SIGUSR1);
   return 0;
 }
